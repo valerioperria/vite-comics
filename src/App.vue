d1b51cf5488f70326e48752ca83653ef649b270d
valerioperria <valerio.perria@gmail.com>
2023-11-23 15:32:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Header from './components/Header.vue';
+import Content from './components/Content.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+export default{
+  components: {
+    Header,
+    Content,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use "./style/general.scss"
+</style>
