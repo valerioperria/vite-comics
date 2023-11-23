6a78e804aa341b66c2ec63954a2a6e41c08d4f33
valerioperria <valerio.perria@gmail.com>
2023-11-23 23:19:58 +0100

    AppObjects

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import AppHeader from './components/AppHeader.vue';
 import AppContent from './components/AppContent.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import AppObjects from './components/AppObjects.vue';
 
 export default {
   components: {
@@ -10,13 +11,15 @@ export default {
     AppContent,
     AppMain,
     AppFooter,
-  },
+    AppObjects
+},
 };
 </script>
 
 <template>
   <AppHeader />
   <AppContent />
+  <AppObjects/>
   <AppMain />
   <AppFooter />
 </template>
---
+++ src/components/AppObjects.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+    data() {
+        return {
+            objects: [
+                {
+                    name: 'DIGITAL COMICS',
+                    object: 'comics-digital-comics.png',
+                },
+                {
+                    name: 'DC MERCHANDISE',
+                    object: 'comics-merchandise.png',
+                },
+                {
+                    name: 'SUBSCRIPTION',
+                    object: 'comics-subscriptions.png',
+                },
+                {
+                    name: 'COMIC SHOP LOCATOR',
+                    object: 'comics-shop-locator.png',
+                },
+                {
+                    name: 'DC POWER VISA',
+                    object: 'dc-power-visa.svg',
+                },
+            ]
+        }
+    },
+    methods: {
+        getImagePath: function (img) {
+            return new URL(`../assets/img/buy-${img}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <ul>
+            <li v-for="item in objects">
+                <img :src="getImagePath(item.object)" alt="">
+                <span>{{ item.name }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+
+.container {
+    background-color: $primaryColor;
+    color: white;
+    width: 100%;
+    height: 140px;
+    padding: 0 120px;
+    display: flex;
+    align-items: center;
+
+    ul {
+        display: flex;
+        list-style-type: none;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+
+        li {
+            display: flex;
+            align-items: center;
+
+            img {
+                height: 3rem;
+                margin-right: 1rem;
+            }
+        }
+    }
+}</style>
